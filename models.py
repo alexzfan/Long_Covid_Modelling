f409f6fdca615d80b7a04c12064a97a8f852afc7
Alex Fan <alexfan@stanford.edu>
2022-11-17 20:40:13 -0800

    starter

---
+++ models.py
@@ -0,0 +1,41 @@
+"""
+Models class
+
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class baseline_ff(nn.Module):
+    """
+    Baseline model for the Long Covid Modeling
+
+    Trains a feed forward network from scratch
+
+    Args:
+        hidden_size,
+        drop_prob
+    """
+    
+    def __init__(self, hidden_size, drop_prob = 0.1):
+        super(baseline_ff, self).__init__()
+
+        self.fc1 = nn.Linear(23074, hidden_size)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.relu2 = nn.ReLU()
+        self.fc3 = nn.Linear(hidden_size, 1)
+        self.dropout = nn.Dropout(drop_prob)
+
+    def forward(self, x):
+
+        # forward through linear layers
+        out = self.fc1(x)
+        out = self.relu1(out)      
+        out = self.relu2(self.fc2(out))  
+        out = self.dropout(out)
+        
+        out = self.fc3(out)
+
+        return out
